bb483f1af86818f11c3e0d28f61ae9210acffdf5
Matthew Lennon <matt.lennon@missguided.com>
2021-10-01 14:32:39 +0100

    Better logging from within rust, panic stacktrace to the console & some example tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,6 @@
 mod utils;
 
-use core::fmt;
+use core::{fmt, panic};
 
 use wasm_bindgen::prelude::*;
 
@@ -22,6 +22,13 @@ pub struct Universe {
 #[wasm_bindgen]
 impl Universe {
     pub fn new() -> Universe {
+        utils::set_panic_hook();
+
+        // let message = String::from("I like dogs");
+        // unsafe {
+        //     log!("{}", message);
+        // }
+
         let width = 64;
         let height = 64;
 
@@ -140,6 +147,39 @@ impl Universe {
     pub fn cells(&self) -> *const Cell {
         self.cells.as_ptr()
     }
+
+    /// Set the width of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_width(&mut self, width: u32) {
+        self.width = width;
+        self.cells = (0..width * self.height).map(|_i| Cell::Dead).collect();
+    }
+
+    /// Set the height of the universe.
+    ///
+    /// Resets all cells to the dead state.
+    pub fn set_height(&mut self, height: u32) {
+        self.height = height;
+        self.cells = (0..self.width * height).map(|_i| Cell::Dead).collect();
+    }
+}
+
+impl Universe {
+    /// Get the dead and alive values of the entire universe.
+    pub fn get_cells(&self) -> &[Cell] {
+        &self.cells
+    }
+
+    /// Set cells to be alive in a universe by passing the row and column
+    /// of each cell as an array.
+    pub fn set_cells(&mut self, cells: &[(u32, u32)]) {
+        for (row, col) in cells.iter().cloned() {
+            let idx = self.get_index(row, col);
+            self.cells[idx] = Cell::Alive;
+        }
+    }
+
 }
 
 impl fmt::Display for Universe {
